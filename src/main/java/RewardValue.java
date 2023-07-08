476c12a885c6f7f4dc8bd4343697bb6b5b4f02cf
abubakrh <bukihussien@gmail.com>
2023-07-08 00:44:57 -0400

    Added RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,17 +1,13 @@
 public class RewardValue {
     private double cashValue;
-    private double milesValue;
 
     // Constructor that accepts a cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue / 0.0035;
     }
 
-    // Constructor that accepts a value in miles as an int
-    public RewardValue(int miles) {
-        this.milesValue = miles;
-        this.cashValue = miles * 0.0035;
+    public RewardValue(int milesValue) {
+        this.cashValue = convert_from_miles_to_cash(milesValue);
     }
 
     // Getter for cashValue
@@ -19,8 +15,15 @@ public class RewardValue {
         return cashValue;
     }
 
-    // Getter for milesValue
-    public double getMilesValue() {
-        return milesValue;
+    public int getMilesValue() {
+        return convert_from_cash_to_miles(this.cashValue);
+    }
+
+    private static int convert_from_cash_to_miles(double cashValue) {
+        return (int) (cashValue / 0.0035);
+    }
+
+    private static double convert_from_miles_to_cash(int milesValue) {
+        return milesValue * 0.0035;
     }
 }
